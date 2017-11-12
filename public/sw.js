02c7a77f537ecd37d363eda5146400e3965f885c
schemr <schemryj@gmail.com>
2017-11-12 12:04:30 +0900

    Add cache check logic in fetch event

--- public/sw.js
+++ public/sw.js
@@ -16,5 +16,14 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
     console.log('[Service Worker] Fetching Data', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if(response) {
+                    return response;
+                }else {
+                    return fetch(event.request);
+                }
+            })
+        )
 });
