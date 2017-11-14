871e168fa286a66a656251939821b467f0664305
schemr <schemryj@gmail.com>
2017-11-14 23:14:46 +0900

    Change static file array variable transformation

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
 var CACHE_STATIC = 'static-v1';
 var CACHE_DYNAMIC = 'dynamic-v1';
+var STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/promise.js',
+    '/src/js/fetch.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Installing Service Worker', event);
     event.waitUntil(
         caches.open(CACHE_STATIC)
             .then(function(cache) {
-                console.log('[Service Worker] App Shell precachching')
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/promise.js',
-                    '/src/js/fetch.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/main.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-                ])
+                console.log('[Service Worker] App Shell precachching');
+                cache.addAll(STATIC_FILES);
             })
     )
 });
