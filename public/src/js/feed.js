38467a8ccc7a20216678f9622ff75d6b5820cf1d
schemr <schemryj@gmail.com>
2017-11-25 21:59:18 +0900

    Add create card UI

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -2,6 +2,9 @@ var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
 var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
     createPostArea.style.transform = 'translateY(0)';
@@ -30,3 +33,25 @@ var url = "https://test-183c9.firebaseio.com/posts"
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard(data) {
+    var cardWrapper = document.createElement('div');
+    cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+    var cardTitle = document.createElement('div');
+    cardTitle.className = 'mdl-card__title';
+    cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+    cardTitle.style.backgroundSize = 'cover';
+    cardWrapper.appendChild(cardTitle);
+    var cardTitleTextElement = document.createElement('h2');
+    cardTitleTextElement.style.color = 'white';
+    cardTitleTextElement.className = 'mdl-card__title-text';
+    cardTitleTextElement.textContent = data.title;
+    cardTitle.appendChild(cardTitleTextElement);
+    var cardSupportingText = document.createElement('div');
+    cardSupportingText.className = 'mdl-card__supporting-text';
+    cardSupportingText.textContent = data.location;
+    cardSupportingText.style.textAlign = 'center';
+    cardWrapper.appendChild(cardSupportingText);
+    componentHandler.upgradeElement(cardWrapper);
+    sharedMomentsArea.appendChild(cardWrapper);
+  }
