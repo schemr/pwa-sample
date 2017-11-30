aa3c9223b99767c1760d8d4c42a2b9cfab5568b1
schemr <schemryj@gmail.com>
2017-11-30 20:27:47 +0900

    Add server side code (capture image send, raw location)

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,25 @@ var functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
 var webpush = require('web-push');
+var formidable = require('formidable');
+var fs = require('fs');
+var UUID = require('uuid-v4');
+var os = require('os');
+var Busboy = require('busboy');
+var path = require('path');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
 var serviceAccount = require("./sample-fb-key.json");
+
+var gcConfig = {
+    projectId: 'test-183c9',
+    keyFilename: 'sample-fb-key.json'
+};
+
+var gcs = require('@google-cloud/storage')(gcConfig);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -16,39 +29,87 @@ admin.initializeApp({
 
 exports.storePostData = functions.https.onRequest(function (request, response) {
     cors(request, response, function () {
-        admin.database().ref('posts').push({
-            id: request.body.id,
-            title: request.body.title,
-            location: request.body.location,
-            image: request.body.image
-        })
-            .then(function () {
-                webpush.setVapidDetails('mailto:schemryj@gmail.com', 'BOSqBClHMUXVve0KMGs6EOUR37lcpp55KIwvNCQU68_beXcxeCvchyhOLo7_o3uWU1lPx5ZHIGgaAbgoq6NIpr4', 'D7PA2t9hoZe9-mseeR6W3yOiQxLSTjJYBQgAqavwFx8');
-                return admin.database().ref('subscriptions').once('value');
-            })
-            .then(function (subscriptions) {
-                subscriptions.forEach(function (sub) {
-                    var pushConfig = {
-                        endpoint: sub.val().endpoint,
-                        keys: {
-                            auth: sub.val().keys.auth,
-                            p256dh: sub.val().keys.p256dh
-                        }
-                    };
+        var uuid = UUID();
+        var busboy = new Busboy({headers: request.headers});
+        // These objects will store the values (file + fields)extracted from busboy
+        var upload;
+        var fileds = {};
 
-                    webpush.sendNotification(pushConfig, JSON.stringify({
-                        title: 'New Post',
-                        content: 'New Post added!',
-                        openUrl: '/help'
-                    }))
-                    .catch(function (err) {
-                        console.log(err);
+        // This callback will be invoked for each file uploaded
+        busboy.on("file", function(fieldname, file, filename, encoding, mimetype) {
+            console.log(`File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`);
+            var filepath = path.join(os.tmpdir(), filename);
+            upload = { file: filepath, type: mimetype };
+            file.pipe(fs.createWriteStream(filepath));
+        });
+        // This will invoked on every field detected
+        busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+                fields[fieldname] = val;
+        });
+
+        // This callback will be invoked after all uploaded files are saved.
+        busboy.on("finish", function() {
+            var bucket = gcs.bucket('test-183c9.appspot.com');
+
+            bucket.upload('/tmp/' + files.file.name, {
+                uploadType: 'media',
+                metadata: {
+                    metadata: {
+                        contentType: files.file.type,
+                        firebaseStorageDownloadTokens: uuid
+                    }
+                }
+            }, function(err, file) {
+                if(!err){
+                    admin.database().ref('posts').push({
+                        id: fields.id,
+                        title: fields.title,
+                        location: fields.location,
+                        rawLocation: {
+                            lat: fields.rawLocationLat,
+                            lng: fields.rawLocationLng
+                        },
+                        image: 'https://firebasestorage.googleapis.com/v0/b/' + bucket.name + '/o/' + encodeURIComponent(file.name) + '?alt=media&token=' + uuid
                     })
-                });
-                response.status(201).json({message: 'Data stored', id: request.body.id});
-            })
-            .catch(function (err) {
-                response.status(500).json({error: err});
+                        .then(function () {
+                            webpush.setVapidDetails('mailto:schemryj@gmail.com', 'BOSqBClHMUXVve0KMGs6EOUR37lcpp55KIwvNCQU68_beXcxeCvchyhOLo7_o3uWU1lPx5ZHIGgaAbgoq6NIpr4', 'D7PA2t9hoZe9-mseeR6W3yOiQxLSTjJYBQgAqavwFx8');
+                            return admin.database().ref('subscriptions').once('value');
+                        })
+                        .then(function (subscriptions) {
+                            subscriptions.forEach(function (sub) {
+                                var pushConfig = {
+                                    endpoint: sub.val().endpoint,
+                                    keys: {
+                                        auth: sub.val().keys.auth,
+                                        p256dh: sub.val().keys.p256dh
+                                    }
+                                };
+            
+                                webpush.sendNotification(pushConfig, JSON.stringify({
+                                    title: 'New Post',
+                                    content: 'New Post added!',
+                                    openUrl: '/help'
+                                }))
+                                .catch(function (err) {
+                                    console.log(err);
+                                })
+                            });
+                            response.status(201).json({message: 'Data stored', id: fields.id});
+                        })
+                        .catch(function (err) {
+                            response.status(500).json({error: err});
+                        });
+                }else{
+                    console.log(err);
+                }
             });
+        });
+        // The raw bytes of the upload will be in request.rawBody.  Send it to busboy, and get
+        // a callback when it's finished.
+        busboy.end(request.rawBody);
+        // formData.parse(request, function(err, fields, files) {
+        //   fs.rename(files.file.path, "/tmp/" + files.file.name);
+        //   var bucket = gcs.bucket("YOUR_PROJECT_ID.appspot.com");
+        // });
     });
 });
