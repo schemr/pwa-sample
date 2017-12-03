b6b62c42fd4ba1416356a80f5d67af9cbad2982e
schemr <schemryj@gmail.com>
2017-12-03 22:42:27 +0900

    Add custom workbox sw-custom.js and add dynamic cache(font,css,image)

---
+++ public/sw-custom.js
@@ -0,0 +1,17 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+    cacheNmae: 'google-fonts'
+}));
+
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css', workboxSW.strategies.staleWhileRevalidate({
+    cacheNmae: 'material-design-css'
+}));
+
+workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis)\.com.*$/, workboxSW.strategies.staleWhileRevalidate({
+    cacheNmae: 'post-images'
+}));
+
+workboxSW.precache([]);
--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -4,6 +4,7 @@ module.exports = {
     "**/*.{html,ico,json,css,js}",
     "src/images/*.{jpg, png}"
   ],
+  "swSrc": "publick/sw-custom.js",
   "swDest": "public/service-worker.js",
   "globIgnores": [
     "../workbox-cli-config.js",
