8707634b7cb71ea324e88a8e05120cec60a133cb
schemr <schemryj@gmail.com>
2017-12-05 19:57:43 +0900

    Add workbox router registerRoute offline html

--- public/sw-custom.js
+++ public/sw-custom.js
@@ -38,4 +38,30 @@ workboxSW.router.registerRoute('https://test-183c9.firebaseio.com/posts', functi
         })
 });
 
+workboxSW.router.registerRoute(function(routeData) {
+    return (routeData.event.request.headers.get('accept').includes('text/html'));
+}, function(args) {
+    return caches.match(args.event.request)
+        .then(function(response) {
+            if(response) {
+                return response;
+            }else {
+                return fetch(args.event.request)
+                    .then(function(res) {
+                        return caches.open('dynamic')
+                            .then(function(cache) {
+                                cache.put(args.event.request.url, res.clone());
+                                return res;
+                            })
+                    })
+                    .catch(function(err) {
+                        return caches.open('/offline.html')
+                            .then(function(cache) {
+                                return cache;
+                            })
+                    });
+            }
+        })
+});
+
 workboxSW.precache([]);
