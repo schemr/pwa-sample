e23ff3c6eef07499b6302d9e1b84434cea7e7622
schemr <schemryj@gmail.com>
2017-12-05 20:04:57 +0900

    Edit dirctory typo

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -4,7 +4,7 @@ module.exports = {
     "**/*.{html,ico,json,css,js}",
     "src/images/*.{jpg, png}"
   ],
-  "swSrc": "publick/sw-custom.js",
+  "swSrc": "public/sw-custom.js",
   "swDest": "public/service-worker.js",
   "globIgnores": [
     "../workbox-cli-config.js",
